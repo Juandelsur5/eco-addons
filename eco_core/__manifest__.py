289d8967e7a8cf7b40f7b3facb74018a4d319d0d
juandelsur5 <juandelsur8@gmail.com>
2025-12-31 18:51:56 -0500

    feat: add eco_core container with canonical menu slots

---
+++ eco_core/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "ECO - Módulo Técnico",
+    "version": "17.0.1.0.0",
+    "summary": "Gobierno técnico, integraciones y permisos delicados",
+    "category": "ECO/Technical",
+    "author": "ECO",
+    "depends": ["base"],
+    "data": [
+        "security/eco_tecnico_groups.xml",
+        "security/ir.model.access.csv",
+        "views/eco_tecnico_menu.xml",
+        "views/eco_tecnico_integration_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
